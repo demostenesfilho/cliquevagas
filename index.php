4f1ae5ff2cb56361f5dfac2f8334ea6f5a7f658f
demostenesfilho <tecnologiasone@gmail.com>
2021-04-14 01:47:40 -0300

    ajuste tela de cidades

--- index.php
+++ index.php
@@ -7,23 +7,20 @@
     <title>Clique Vagas :: Escolha sua cidade!</title>
 </head>
 <body>
-    <div class="tela">
+    <div class="conteudo">
 
-        <div class="conteudo">
-
-            <div class="conteudo-esquerda">
-            <h4>Clique Vagas</h4>
-            <h1>Escolha sua cidade</h1>
-            <p>Escolha a cidade que você deseja conseguir sua nova oportunidade de emprego 
-                e realize seu sonho de trabalhar com o que você ama, onde você se sente bem!</p>
-            </div>
-
-            <div class="conteudo-direita">
-            
-            </div>
-
-
+        <div class="conteudo-esquerda">
+        <h4>Clique Vagas</h4>
+        <h1>Escolha sua cidade</h1>
+        <p>Escolha a cidade que você deseja conseguir sua nova oportunidade de emprego 
+            e realize seu sonho de trabalhar com o que você ama, onde você se sente bem!</p>
         </div>
+
+        <div class="conteudo-direita">
+        <button class="btn-caruaru"><a href="caruaru/index.php">Caruaru</a></button>
+        <button class="btn-recife"><a href="recife/index.php">Recife</a></button>
+        </div>
+        
     </div>
 </body>
 </html>
