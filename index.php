38ccf47ce5a961888f6cc3762b39125ec0170bc9
demostenesfilho <tecnologiasone@gmail.com>
2021-04-14 00:03:57 -0300

    ajuste

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escolha sua cidade!</title>
+</head>
+<body>
+    <h1>Escolha sua cidade</h1>
+    <a href="caruaru/index.php">caruaru</a>
+    <a href="recife/index.php">Recife</a>
+</body>
+</html>
