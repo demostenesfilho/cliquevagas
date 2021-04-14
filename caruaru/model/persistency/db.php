46150013920a66abe5d9b27e340767cb90725159
demostenesfilho <tecnologiasone@gmail.com>
2021-04-14 02:27:46 -0300

    ajuste

--- caruaru/model/persistency/db.php
+++ caruaru/model/persistency/db.php
@@ -1,6 +1,6 @@
 <?php
 putenv("DATABASE_URL=postgres://tjkffkenqbwmvd:155f089e921c8b63afebc5559cbb884a5512076e7414f523796b325c892f60c8@ec2-3-233-43-103.compute-1.amazonaws.com:5432/dic40ha3gf799");
-//echo getenv("DATABASE_URL");
+echo getenv("DATABASE_URL");
 
 /* CONEXÃO DE BANCO SQL */
 function banco($sql){
