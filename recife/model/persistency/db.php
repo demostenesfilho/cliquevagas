8e4be3890de2cf9bcfadb31898e8ae7abe8dc6d7
Demóstenes Filho <tecnologiasone@gmail.com>
2021-04-14 23:17:57 -0300

    ajuste

--- recife/model/persistency/db.php
+++ recife/model/persistency/db.php
@@ -1,5 +1,6 @@
 <?php
-//putenv("DATABASE_URL=host=localhost port=5432 dbname=postgres");
+putenv("DATABASE_URL=host=ec2-3-233-43-103.compute-1.amazonaws.com port=5432 user=tjkffkenqbwmvd dbname=dic40ha3gf799 password=155f089e921c8b63afebc5559cbb884a5512076e7414f523796b325c892f60c8");
+//putenv("DATABASE_URL=host=localhost port=5432 user=postgres dbname=postgres password=123");
 //echo getenv("DATABASE_URL");
 
 /* CONEXÃO DE BANCO SQL */
